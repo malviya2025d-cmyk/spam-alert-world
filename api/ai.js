997450e72e66bbaa3ec316b8859adc9e0d23a7fe
malviya2025d-cmyk <malviya2025d@gmail.com>
2026-04-18 18:20:28 +0530

    Create ai.js

---
+++ api/ai.js
@@ -0,0 +1,40 @@
+export default async function handler(req, res) {
+
+  if (req.method !== "POST") {
+    return res.status(405).json({ reply: "Only POST allowed" });
+  }
+
+  const { prompt } = req.body;
+
+  try {
+    const response = await fetch("https://api.groq.com/openai/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${process.env.GROQ_API_KEY}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        model: "llama3-8b-8192",
+        messages: [
+          {
+            role: "system",
+            content: "You are a spam detection AI. Tell if message is spam or safe in short."
+          },
+          {
+            role: "user",
+            content: prompt
+          }
+        ]
+      })
+    });
+
+    const data = await response.json();
+
+    res.status(200).json({
+      reply: data.choices?.[0]?.message?.content || "No response"
+    });
+
+  } catch (error) {
+    res.status(500).json({ reply: "Server error" });
+  }
+}
